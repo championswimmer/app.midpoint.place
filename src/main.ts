6362066feb232cdb9159cc0cbbd2aea56101c500
Arnav Gupta <dev@championswimmer.in>
2025-05-22 03:54:50 +0100

    Add sass as a dependency, update chokidar to version 4.0.3, and enhance base.css with new Bootstrap color definitions and theme configuration imports.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './assets/bootstrap-theme.css'
 import { createBootstrap } from 'bootstrap-vue-next'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
@@ -10,10 +11,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// Add our CSS
-import './assets/main.css'
-
-
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
@@ -21,7 +18,5 @@ pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
 app.use(createBootstrap())
-
-
 
 app.mount('#app')
