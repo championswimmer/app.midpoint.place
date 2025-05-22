64207e01b9645e90e1155d8cefebe69d344ff8f1
Arnav Gupta <dev@championswimmer.in>
2025-05-22 04:00:48 +0100

    Remove TheWelcome component and update HomeView to include a welcome header and description for Midpoint Place.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <header>
+      <h1 class="text-center fw-bold fs-3 my-2">
+        Welcome to Midpoint Place
+      </h1>
+    </header>
+    <section class="container mt-4">
+      <p>
+        Midpoint Place is a platform for finding the midpoint of a place.
+      </p>
+    </section>
   </main>
 </template>
